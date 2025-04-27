f34cc919043038e0c431c2f5b51e80cdc76b01e3
BrunoPoiano <bruno.poiano@proton.me>
2025-04-27 15:06:55 -0300

    edit product passing

--- tests/Feature/Products/EditProductTest.php
+++ tests/Feature/Products/EditProductTest.php
@@ -1,16 +1,166 @@
 <?php
 
+use App\Models\Product;
 use App\Models\User;
 
-test('authenticated users can create products', function () {
+test('authenticated users can edit products', function () {
     $user = User::factory()->create();
     $this->actingAs($user);
 
-    $response = $this->post('/products', [
+    $response = $this->postJson('/products', [
         'name' => 'Test Product',
         'description' => 'Test Description',
         'price' => 100,
         'quantity' => 1
     ]);
     $response->assertStatus(201);
+
+    $product = Product::first();
+    $response = $this->putJson('/products/' . $product->id, [
+        'name' => 'Updated Product',
+        'description' => 'Updated Description',
+        'price' => 200,
+        'quantity' => 2
+    ]);
+    $response->assertStatus(200);
+});
+test('authenticated users cannot edit products without name', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    $response = $this->postJson('/products', [
+        'name' => 'Test Product',
+        'description' => 'Test Description',
+        'price' => 100,
+        'quantity' => 1
+    ]);
+    $response->assertStatus(201);
+
+    $product = Product::first();
+    $response = $this->putJson('/products/' . $product->id, [
+        'description' => 'Updated Description',
+        'price' => 200,
+        'quantity' => 2
+    ]);
+    $response->assertStatus(422);
+});
+
+test('authenticated users can edit products without description', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    $response = $this->postJson('/products', [
+        'name' => 'Test Product',
+        'description' => 'Test Description',
+        'price' => 100,
+        'quantity' => 1
+    ]);
+    $response->assertStatus(201);
+
+    $product = Product::first();
+    $response = $this->putJson('/products/' . $product->id, [
+        'name' => 'Updated Product',
+        'price' => 200,
+        'quantity' => 2
+    ]);
+    $response->assertStatus(200);
+});
+
+test('authenticated users cannot edit products without price', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    $response = $this->postJson('/products', [
+        'name' => 'Test Product',
+        'description' => 'Test Description',
+        'price' => 100,
+        'quantity' => 1
+    ]);
+    $response->assertStatus(201);
+
+    $product = Product::first();
+    $response = $this->putJson('/products/' . $product->id, [
+        'name' => 'Updated Product',
+        'description' => 'Updated Description',
+        'quantity' => 2
+    ]);
+    $response->assertStatus(422);
+});
+
+test('authenticated users cannot edit products without quantity', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    $response = $this->postJson('/products', [
+        'name' => 'Test Product',
+        'description' => 'Test Description',
+        'price' => 100,
+        'quantity' => 1
+    ]);
+    $response->assertStatus(201);
+
+    $product = Product::first();
+    $response = $this->putJson('/products/' . $product->id, [
+        'name' => 'Updated Product',
+        'description' => 'Updated Description',
+        'price' => 200,
+    ]);
+    $response->assertStatus(422);
+});
+
+test('authenticated users cannot edit products that are not found', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    $response = $this->postJson('/products', [
+        'name' => 'Test Product',
+        'description' => 'Test Description',
+        'price' => 100,
+        'quantity' => 1
+    ]);
+    $response->assertStatus(201);
+
+    $product = Product::first();
+    $response = $this->putJson('/products/' . 999, [
+        'name' => 'Updated Product',
+        'description' => 'Updated Description',
+        'price' => 200,
+    ]);
+    $response->assertStatus(404);
+});
+
+
+test('authenticated users cannot edit products that is not his own', function () {
+
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    $response = $this->postJson('/products', [
+        'name' => 'Test Product',
+        'description' => 'Test Description',
+        'price' => 100,
+        'quantity' => 1
+    ]);
+    $response->assertStatus(201);
+
+    $user_product = Product::first();
+    $response = $this->putJson('/products/' . $user_product->id, [
+        'name' => 'Updated Product',
+        'description' => 'Updated Description',
+        'price' => 200,
+        'quantity' => 2
+    ]);
+    $response->assertStatus(200);
+
+    $second_user = User::factory()->create();
+    $this->actingAs($second_user);
+
+    $response = $this->putJson('/products/' . $user_product->id, [
+        'name' => 'Updated Product',
+        'description' => 'Updated Description',
+        'price' => 200,
+        'quantity' => 2
+    ]);
+    $response->assertStatus(403);
+
 });
